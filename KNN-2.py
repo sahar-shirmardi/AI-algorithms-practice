0729786bcafcf41194ce368ac113b8535f3b555b
sahar-shirmardi <sahar.s.shirmardi@gmail.com>
2021-07-21 02:04:27 +0430

    preprocessing

--- KNN-2.py
+++ KNN-2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # Import dataset
 df_train = pd.read_csv('./Survivors/train.csv')
@@ -21,4 +22,17 @@ df_test_set = df_train.drop(['Name',
                             'Parch', 
                             'Age'], 
                             axis=1)
+
+
+# Fill in NaN with mean
+mean = df_test_set['Fare'].mean()
+df_test_set['Fare'] = df_test_set['Fare'].fillna(mean)
+
+# Categorical featureas to numerical 
+labelencoder = LabelEncoder()
+df_train_set.iloc[:, 2] = labelencoder.fit_transform(df_train_set.iloc[:, 2].values)
+df_train_set.iloc[:, 4] = labelencoder.fit_transform(df_train_set.iloc[:, 4].values)
+df_test_set.iloc[:, 2] = labelencoder.fit_transform(df_test_set.iloc[:, 2].values)
+df_test_set.iloc[:, 4] = labelencoder.fit_transform(df_test_set.iloc[:, 4].values)
+
 
